d7b75c43ba1d21e71774c1b88934c8829d0a322d
Hu-868 <1601106027@qq.com>
2025-04-20 11:27:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// 示例：平滑滚动到锚点
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
